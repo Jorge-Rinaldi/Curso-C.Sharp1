46b5882d0f44629a4627cf7dab40a1822429e61e
Jorge-Rinaldi <jorgelrinaldi.jr@gmail.com>
2023-08-26 23:16:46 -0300

    Ejercicio1

---
+++ Unidad2-PrimerPrograma/Ejercicio1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1,n2,resultado;
+
+            Console.WriteLine("Ingrese el primer nro: ");
+            n1=int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo nro: ");
+            n2=int.Parse(Console.ReadLine());
+
+            resultado=n1+n2;
+
+            Console.WriteLine("La suma es: "+resultado);
+    
+        }
+    }
+}
