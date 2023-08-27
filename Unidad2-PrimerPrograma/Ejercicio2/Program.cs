7bdce70c713403ba97c2ec9720362dd9ccda25ea
Jorge-Rinaldi <jorgelrinaldi.jr@gmail.com>
2023-08-26 23:21:30 -0300

    Ejercicio2

---
+++ Unidad2-PrimerPrograma/Ejercicio2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int n, resultado;
+
+           Console.WriteLine("Ingrese el nro: ");
+           n=int.Parse(Console.ReadLine());
+
+           resultado = n * n * n;
+
+           Console.WriteLine("El resultado de elevar "+n+" al cubo es: "+resultado);
+
+        }
+    }
+}
